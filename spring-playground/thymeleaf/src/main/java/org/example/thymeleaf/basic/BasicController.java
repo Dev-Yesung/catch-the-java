e596c06a75ed386322fee862d9918c4823994746
Dev-Yesung <yeasung67@gmail.com>
2024-05-07 02:54:51 +0900

    update thymeleaf

--- spring-playground/thymeleaf/src/main/java/org/example/thymeleaf/basic/BasicController.java
+++ spring-playground/thymeleaf/src/main/java/org/example/thymeleaf/basic/BasicController.java
@@ -5,11 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 
 @Controller
@@ -48,6 +52,29 @@ public class BasicController {
 		model.addAttribute("userMap", map);
 
 		return "/basic/variable";
+	}
+
+	@GetMapping("basic-objects")
+	public String basicObjects(
+		Model model,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		HttpSession session) {
+		// 스프링부트 3.0 이상부터는 타임리프에서 아래의 기본객체를 제공하지 않는다.
+		model.addAttribute("request", request);
+		model.addAttribute("response", response);
+		model.addAttribute("servletContext", request.getServletContext());
+
+		session.setAttribute("sessionData", "Hello Session");
+
+		return "/basic/basic-objects";
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
+		}
 	}
 
 	@Data
